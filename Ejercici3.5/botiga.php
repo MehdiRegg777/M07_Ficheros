20995744b1bf1fb31c092b45644eb44b34efc4ae
MehdiRegg777 <96047417+MehdiRegg777@users.noreply.github.com>
2023-09-28 20:10:16 +0200

    ex3.5

---
+++ Ejercici3.5/botiga.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Botiga</title>
+</head>
+<body>
+    <form>
+        <input type="checkbox" name="prod[]" value="peres">Peres
+        <br>
+        <input type="checkbox" name="prod[]" value="pomes">Pomes
+        <br>
+        <input type="checkbox" name="prod[]" value="pastanagues">Pastanagues
+        <br>
+        <input type="submit">
+    </form>
+
+    <?php
+
+        
+
+
+    ?>
+
+
+</body>
+</html>
